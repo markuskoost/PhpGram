8f41342422ffabccc142fc5e0259bdedb83302d7
markuskoost <33056955+markuskoost@users.noreply.github.com>
2021-03-24 02:18:23 +0200

    Profile UI and added username to registration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row d">
+            <div class="col-3 p-5"></div>
+            <img
+                src="https://instagram.fkwi2-2.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fkwi2-2.fna.fbcdn.net&_nc_ohc=NoLsLSZoKUEAX8bSYm_&ccb=7-4&oh=e349489676d4fcdf07026df0f57c8199&oe=60837F0F&_nc_sid=e2b730&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2-ccb7-4"
+                class="rounded-circle" width="150" height="150">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>PhpGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>153</strong> posts</div>
+                <div class="pr-4"><strong>23k</strong> followers</div>
+                <div class="pr-4"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">PhpGram.com</div>
+            <div>PHP is a general-purpose scripting language especially suited to web development. It was originally
+                created by Danish-Canadian programmer Rasmus Lerdorf in 1994
+            </div>
+            <div><a href="£">www.phpgram.com</a></div>
+        </div>
+    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="row">
+        <div class="col-4">
+            <img
+                src="https://instagram.fkwi2-2.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fkwi2-2.fna.fbcdn.net&_nc_ohc=NoLsLSZoKUEAX8bSYm_&ccb=7-4&oh=e349489676d4fcdf07026df0f57c8199&oe=60837F0F&_nc_sid=e2b730&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2-ccb7-4">
+            <div class="col-4">
+                <img
+                    src="https://instagram.fkwi2-2.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fkwi2-2.fna.fbcdn.net&_nc_ohc=NoLsLSZoKUEAX8bSYm_&ccb=7-4&oh=e349489676d4fcdf07026df0f57c8199&oe=60837F0F&_nc_sid=e2b730&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2-ccb7-4">
+                <div class="col-4">
+                    <img
+                        src="https://instagram.fkwi2-2.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fkwi2-2.fna.fbcdn.net&_nc_ohc=NoLsLSZoKUEAX8bSYm_&ccb=7-4&oh=e349489676d4fcdf07026df0f57c8199&oe=60837F0F&_nc_sid=e2b730&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2-ccb7-4">
                 </div>
             </div>
         </div>
-    </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,21 +10,22 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/php-logo.svg" style="height: 30px; border-right: 1px solid #333;" class="pr-3"></div>
+                    <div class="pl-3 pt-1">PhpGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -54,7 +55,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->username }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
