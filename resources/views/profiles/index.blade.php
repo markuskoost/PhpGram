6bdabe1891c4121bd2e4e987e337cfe5f09ae685
markuskoost <33056955+markuskoost@users.noreply.github.com>
2021-04-11 15:51:57 +0300

    Fix profile UI

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,16 +3,13 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-3 p-5"></div>
-            <img
-                src="{{ $user->profile->profileImage() }}"
-                class="rounded-circle" width="150" height="150" alt="">
-        </div>
-        <div class="col-9 pt-5">
-            <div class="d-flex justify-content-between align-items-baseline">
-                <div class="d-flex align-items-center pb-3">
-                    <div class="d-flex ml-4">
-                       <div class="h4">{{ $user->username}}</div>
+            <div class="col-3 p-5">
+                <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-100">
+            </div>
+            <div class="col-9 pt-5">
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <div class="d-flex align-items-center pb-3">
+                        <div class="h4">{{ $user->username }}</div>
 
                         <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
                     </div>
@@ -28,20 +25,18 @@
                 @endcan
 
                 <div class="d-flex">
-                    <div class="pr-4"><strong>{{$postCount}}</strong> posts</div>
-                    <div class="pr-4"><strong>{{$followersCount}}</strong> followers</div>
-                    <div class="pr-4"><strong>{{$followingCount}}</strong> following</div>
+                    <div class="pr-5"><strong>{{ $postCount }}</strong> posts</div>
+                    <div class="pr-5"><strong>{{ $followersCount }}</strong> followers</div>
+                    <div class="pr-5"><strong>{{ $followingCount }}</strong> following</div>
                 </div>
                 <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
-                <div>
-                    {{ $user->profile->description }}
-                </div>
-                <div><a href="£">{{ $user->profile->url }}</a></div>
+                <div>{{ $user->profile->description }}</div>
+                <div><a href="#">{{ $user->profile->url }}</a></div>
             </div>
         </div>
 
-        <div class="row pt-4">
-            @foreach( $user->posts as $post)
+        <div class="row pt-5">
+            @foreach($user->posts as $post)
                 <div class="col-4 pb-4">
                     <a href="/p/{{ $post->id }}">
                         <img src="/storage/{{ $post->image }}" class="w-100">
